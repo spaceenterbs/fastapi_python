3849f53fcbd0f12e2f04505799bae37031f2c59d
enterspacebs <anotherhell48@gmail.com>
2023-09-07 23:31:00 +0900

    DELETE api

--- todos/models.py
+++ todos/models.py
@@ -13,3 +13,15 @@ class Todo(BaseModel):
             }
         }
     }
+
+
+class TodoItem(BaseModel):  # UPDATE 라우트 요청 바디용 모델
+    item: str
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "item": "Read the next chapter of the book.",
+            }
+        }
+    }
--- todos/todo.py
+++ todos/todo.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Path
-from models import Todo
+from models import Todo, TodoItem
 
 todo_router = APIRouter()
 
@@ -34,4 +34,42 @@ async def get_single_todo(
             }
     return {
         "message": "Todo with supplied ID doesn't exist.",
+    }
+
+
+@todo_router.put("/todo/{todo_id}")
+async def update_todo(
+    todo_data: TodoItem,
+    todo_id: int = Path(..., title="The ID of the todo to be updated."),
+) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+            todo.item = todo_data.item
+            return {
+                "message": "Todo updated successfully.",
+            }
+    return {
+        "message": "Todo with supplied ID doesn't exist.",
+    }
+
+
+@todo_router.delete("/todo/{todo_id}")
+async def delete_single_todo(todo_id: int) -> dict:
+    for index in range(len(todo_list)):
+        todo = todo_list[index]
+        if todo.id == todo_id:
+            todo_list.pop(index)
+        return {
+            "message": "Todo deleted successfully.",
+        }
+    return {
+        "message": "Todo with supplied ID doesn't exist.",
+    }
+
+
+@todo_router.delete("/todo")
+async def delete_all_todos() -> dict:
+    todo_list.clear()
+    return {
+        "message": "Todos deleted successfully.",
     }
