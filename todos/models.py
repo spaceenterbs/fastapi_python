314d2a54cc84040a7937fdc2113ba64ede875b99
enterspacebs <anotherhell48@gmail.com>
2023-09-07 22:44:13 +0900

    pydantic v2 swagger and redoc

--- todos/models.py
+++ todos/models.py
@@ -4,3 +4,12 @@ from pydantic import BaseModel
 class Todo(BaseModel):
     id: int
     item: str
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "id": 1,
+                "item": "Example Schema!",
+            }
+        }
+    }
--- todos/todo.py
+++ todos/todo.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path
 from models import Todo
 
 todo_router = APIRouter()
@@ -22,7 +22,11 @@ async def retrieve_todos() -> dict:
 
 
 @todo_router.get("/todo/{todo_id}")  # todo_id를 경로 매개변수로 추가한다.
-async def get_single_todo(todo_id: int) -> dict:
+async def get_single_todo(
+    todo_id: int = Path(
+        ..., title="The ID of the todo to retrieve."
+    )  # Path는 라우트 함수에 있는 "다른 인수와 경로 매개변수를 구분"하는 역할을 한다.
+) -> dict:
     for todo in todo_list:
         if todo.id == todo_id:
             return {
