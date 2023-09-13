55150f38d0e34e850e94a7fc62b40d3b4b5761d4
enterspacebs <anotherhell48@gmail.com>
2023-09-13 22:46:06 +0900

    CORS 주석 추가함

--- planner/main.py
+++ planner/main.py
@@ -24,14 +24,17 @@ app.include_router(
 
 # 출처 등록
 
-origins = ["*"]
+# origins 배열을 등록하고
+origins = ["*"]  # 어떤 출처(origin)에서 들어오는 요청을 허용할지 설정한다.
 
+# add_middleware() 메서드를 사용해 미들웨어를 등록한다.
+# 미들웨어는 각 요청과 응답 사이에서 작동하는 컴포넌트로, 여러 가지 기능(로그 생성, 오류 처리 등)을 수행할 수 있다.
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    CORSMiddleware,  # 추가하려는 미들웨어의 종류. 여기서는 CORS 관련 미들웨어를 사용한다.
+    allow_origins=origins,  # 어떤 출처의 요청을 허용할지 결정한다. 위에서 정의한 origins 리스트를 사용한다.
+    allow_credentials=True,  # 자격 증명(예: 쿠키나 HTTP Authentication 데이터)을 포함한 요청도 허용할지 결정한다.
+    allow_methods=["*"],  # 어떤 HTTP 메서드(GET, POST 등)를 허용할지 결정한다.
+    allow_headers=["*"],  # 어떤 HTTP 헤더를 허용할지 결정한다.
 )
 
 
